05d199f42600e635707710e1b99b23c225e936ea
HiramZednem <hirammendez000@gmail.com>
2023-11-04 09:58:49 -0600

    registro agregado

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent{
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
 
-  ngOnInit(): void {
+  constructor(private AuthService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    console.log('submit');
+    this.AuthService.login(this.email, this.password).subscribe(
+      (response) => {
+        if (response === true) {
+          // Redirige al usuario a la ruta "downloads"
+          this.router.navigate(['/download']);
+          localStorage.setItem('autenticated', 'true');
+        } else {
+          alert('Usuario o contraseña incorrectos');
+        }
+      });
   }
 
 }
